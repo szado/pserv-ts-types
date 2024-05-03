25d5e85e78199247cdc9edf0c1040aec3d84fb94
Jarosław Żak <shado.ackerly@gmail.com>
2024-05-03 23:15:21 +0200

    fix: support for empty followed topics reply array

--- src/schemes/events/FollowedTopics.ts
+++ src/schemes/events/FollowedTopics.ts
@@ -1,5 +1,7 @@
 import {FollowedTopic} from "../FollowedTopic";
+import {ChatLocation} from "../ChatLocation";
 
 export interface FollowedTopics {
+    location: ChatLocation;
     followedTopics: FollowedTopic[];
 }
