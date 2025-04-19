250b2798cad3d9085f86ecd5075cf2129f066d47
Jarek <jaroslaw.zak@sky-shop.pl>
2025-04-19 12:58:46 +0200

    CustomNickChange message type

--- src/schemes/Message.ts
+++ src/schemes/Message.ts
@@ -1,7 +1,7 @@
 import {User} from "./User";
 import {ChatLocation} from "./ChatLocation";
 
-export type MessageType = 'Text'|'RoomJoin'|'RoomLeave'|'SpaceJoin'|'SpaceLeave'|'TopicChange';
+export type MessageType = 'Text'|'RoomJoin'|'RoomLeave'|'SpaceJoin'|'SpaceLeave'|'TopicChange'|'CustomNickChange';
 
 export interface MessageAuthor {
     user: User;
